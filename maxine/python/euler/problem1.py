f5682ae6838e37684806c419abd2144b4d9eaeb6
maxinems <maxinemalekhi@hotmail.com>
2020-07-22 12:14:18 +0700

    project euler

---
+++ maxine/python/euler/problem1.py
@@ -0,0 +1,6 @@
+"if we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000"
+
+numbers = [x for x in range (1,1000) if x % 5 == 0 or x % 3 == 0 ]
+sum (numbers)
