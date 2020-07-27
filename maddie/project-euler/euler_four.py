7ee26e8a7c11e3dc8a63141127e3d4008091f87f
maddieprmdy <maddie@zero-one-group.com>
2020-07-27 14:13:04 +0700

    Added fourth euler

---
+++ maddie/project-euler/euler_four.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+n = 0
+for a in range(999, 100, -1):
+    for b in range(a, 100, -1):
+        x = a * b
+        if x > n:
+            s = str(a * b)
+            if s == s[::-1]:
+                n = a * b
+print(n)
