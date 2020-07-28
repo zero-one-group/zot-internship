2da84fa84ae82c6008c83c8093664576269390ec
maxinems <maxinemalekhi@hotmail.com>
2020-07-28 20:19:09 +0700

    add problem16

--- maxine/python/euler/problem16.py
+++ maxine/python/euler/problem16.py
@@ -1,4 +1,3 @@
 """215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. What is the sum of the digits of the number 21000?"""
-def two_power_nth(n):
-    return 2**(n)
-sum(int(i) for i in str(two_power_nth(1000)))
+
+sum(int(i) for i in str(2**(1000)))
