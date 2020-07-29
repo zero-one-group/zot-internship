ecc50dacda6133a1fbd7b283d91e786436a0fe2c
maxinems <maxinemalekhi@hotmail.com>
2020-07-29 11:04:46 +0700

    revised problem 10

--- maxine/python/euler/problem10.py
+++ maxine/python/euler/problem10.py
@@ -1,11 +1,15 @@
-"The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+"""The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million."""
 
-Find the sum of all the primes below two million."
-
-def is_prime(n):
-    if n>1:
-        for d in range (2,n):
-            if n % d ==0:
-                return False
+import math
+def is_prime(number):
+    if number == 2:
         return True
+    if number == 1 or number % 2 == 0:
+        return False 
+    for i in range(3, (int(math.sqrt(number)) + 1), 2):
+        if number % i == 0:
+            return False;
+    return True  
+
 sum(i for i in range(2000000) if is_prime(i))
