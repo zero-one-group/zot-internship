9e53acb825a5e20d33c0613e721de760659807ec
maxinems <maxinemalekhi@hotmail.com>
2020-07-29 12:35:55 +0700

    revised problem 3

--- maxine/python/euler/problem3.py
+++ maxine/python/euler/problem3.py
@@ -1,11 +1,9 @@
-"find largest prime factor of 600851475143"
-def prime_factors(n):
-    for x in range (2,n):
-         if n % x ==0:
-             n = n/x
-             for i in range (2,n):
-                 while n %i ==0:
-                     n = n/i
-                     yield i
-                yield x 
-max(prime_factors(13195))
+"""The prime factors of 13195 are 5, 7, 13 and 29.find largest prime factor of
+600851475143"""
+import math
+def factors_of_n(num):
+    return (int(num/x) for x in range(1,num) if num % x == 0)
+def prime_factors_of_n(num):
+    for factor in factors_of_n(num):
+        return (i for i in range(3, int(math.sqrt(factor)) + 1, 2) if factor % i == 0 )
+max(prime_factors_of_n(600851475143))
