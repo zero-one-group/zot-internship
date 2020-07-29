48c3eead93a6a174dccc986d47f9ff1c7f8dbc31
Andrew H. Widjaja <AHW@macbook-pro-2.mynet>
2020-07-29 10:56:15 +0100

    Edited indentation based PEP 8

--- andrew/demo_code/animal.py
+++ andrew/demo_code/animal.py
@@ -1,22 +1,22 @@
 import sys
 
 def cat():
-	print('meow!')
+    print('meow!')
 
 def default():
-	print('hello')
+    print('hello')
 
 def dog():
-	print('woof!')
+    print('woof!')
 
 def main():
-	if sys.argv[1] == 'cat':
-		cat()
-	elif sys.argv[1] == 'dog':
-		dog()	
-	else:
-		default()
+    if sys.argv[1] == 'cat':
+	cat()
+    elif sys.argv[1] == 'dog':
+	dog()	
+    else:
+	default()
 
 if __name__ == '__main__':
-	main()
+    main()
 
