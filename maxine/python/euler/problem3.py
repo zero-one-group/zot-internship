22d62f890ab34ab29566d587620a27471b170763
maxinems <maxinemalekhi@hotmail.com>
2020-07-30 12:47:21 +0700

    added PEP8 blank lines

--- maxine/python/euler/problem3.py
+++ maxine/python/euler/problem3.py
@@ -1,9 +1,15 @@
 """The prime factors of 13195 are 5, 7, 13 and 29.find largest prime factor of
 600851475143"""
 import math
+
+
 def factors_of_n(num):
-    return (int(num/x) for x in range(1,num) if num % x == 0)
+    return (int(num/x) for x in range(1, num) if num % x == 0)
+
+
 def prime_factors_of_n(num):
     for factor in factors_of_n(num):
-        return (i for i in range(3, int(math.sqrt(factor)) + 1, 2) if factor % i == 0 )
+        return (i for i in range(3, int(math.sqrt(num)) + 1) if factor % i == 0 )
+
+
 max(prime_factors_of_n(600851475143))
