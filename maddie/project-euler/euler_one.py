5523ee50482e09de114178b5dd73ea49b4245c4f
maddieprmdy <67698655+maddieprmdy@users.noreply.github.com>
2020-08-01 18:31:24 +0700

    Update euler_one.py

--- maddie/project-euler/euler_one.py
+++ maddie/project-euler/euler_one.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-total = 0
-for i in range(1, 1000):
-    if (i % 3 == 0 or i % 5 == 0):
-        total = total + i
-print(total)
+sum([
+    x
+    for x in range(1, 1001)
+    if x % 3 == 0 or x % 5 == 0
+])
