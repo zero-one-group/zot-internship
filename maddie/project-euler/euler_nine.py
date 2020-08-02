f6795fcf6b9dbf88f64e46f6aac2fcf20332bb95
maddieprmdy <67698655+maddieprmdy@users.noreply.github.com>
2020-08-02 12:03:54 +0700

    Update euler_nine.py

--- maddie/project-euler/euler_nine.py
+++ maddie/project-euler/euler_nine.py
@@ -6,8 +6,8 @@ def pythag_triplet():
             c = 1000 - a - b
 
             if a**2 + b**2 == c**2:
-                print("a = %d, b = %d, c = %d. abc = %d" % (a, b, c, a * b * c))
-                return
+                return a, b, c
 
 pythag_triplet()
+print("a:", a, "b:", b, "c:", c)
 
