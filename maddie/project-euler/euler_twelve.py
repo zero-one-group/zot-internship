173ebba0de7f4ca8d21a16f2b2c73fbd511ade20
maddieprmdy <67698655+maddieprmdy@users.noreply.github.com>
2020-08-02 22:58:40 +0700

    Update euler_twelve.py

--- maddie/project-euler/euler_twelve.py
+++ maddie/project-euler/euler_twelve.py
@@ -2,7 +2,7 @@
 #what is the first triangle number to have over 500 divisors
 
 def triangle_number(n):
-    return sum([i for i in range(1, n + 1)])
+    return sum([iterator for iterator in range(1, n + 1)])
 
 number = 0
 result = 0
