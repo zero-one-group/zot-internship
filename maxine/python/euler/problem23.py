5909522c05c21a5a658c5d3a61d909652d71aede
maxinems <maxinemalekhi@hotmail.com>
2020-08-10 11:31:30 +0700

    modified p23

--- maxine/python/euler/problem23.py
+++ maxine/python/euler/problem23.py
@@ -1,16 +1,14 @@
-"""Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers."""
+"""Find the sum of all the positive integers which cannot be written as the
+sum of two abundant numbers."""
 
 
-def is_abundant(x):
-    if x > sum(i for i in range (1, x) if x % i == 0):
+def is_abundant(num):
+    if num > sum(i for i in range(1, num) if num % i == 0):
         return True
-    else:
-        return False
-        
 
-def cannot_be_written_as_two_abundant(x):
-    return (i for i in range(x) if i is not is_abundant(int(i/2)))
+
+def cannot_be_written_as_two_abundant(num):
+    return (i for i in range(num) if i is not is_abundant(int(i/2)))
 
 
 sum(cannot_be_written_as_two_abundant(28123))
-
