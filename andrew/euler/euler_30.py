f33925577d22ffd5865fcf227a7132142ee542f8
Andrew H. Widjaja <AHW@macbook-pro-2.mynet>
2020-08-11 19:09:56 +0100

    Added Euler 30

---
+++ andrew/euler/euler_30.py
@@ -0,0 +1,16 @@
+'''
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+'''
+
+def power(list, power):
+    return [ x**power for x in list ]
+
+
+total = 0
+for num in range(2, 350000):
+    digits = [int(digit) for digit in str(num)]
+    answer = power(digits, 5)
+    if sum(answer) == num:
+        total += num
+
+print("Sum =", total)
