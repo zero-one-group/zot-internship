145e211581bc37243b2f40b557a68d5e39588f38
maxinems <maxinemalekhi@hotmail.com>
2020-08-27 15:14:27 +0700

    stats exercise

--- maxine/stats/exercise_stats_6.py
+++ maxine/stats/exercise_stats_6.py
@@ -12,12 +12,19 @@ from scipy import stats
 def beta_dist(alpha, beta, draws):
     return np.random.beta(alpha, beta, size=draws)
 
+
 def confidence_interval_mean(dist, alpha_ci):
     return stats.mstats.trimmed_mean_ci(dist, alpha=alpha_ci)
-    #mean, var, std = stats.bayes_mvs(dist, alpha=ci)
 
-plt.plot(beta_dist(1, 2, 100))
-plt.savefig("beta.png")
 
-list(confidence_interval_mean(beta_dist(1, 2, 100), 0.05) for _ in range(1000)) 
+nsims = 1000
+ninety_five_conf_interval = list(confidence_interval_mean(beta_dist(1, 2, 100), 0.05) for _ in range(nsims)) 
+true_mean = 1/3
+interval_with_true_mean = [ninety_five_conf_interval[index] for index in range(len(ninety_five_conf_interval)) if
+     ninety_five_conf_interval[index][0] < true_mean <
+     ninety_five_conf_interval[index][1]]
+proportion = len(interval_with_true_mean) / len(ninety_five_conf_interval) * 100
+print("proportion of interval with true mean: %d percent" % (round(proportion, 3)))
+print("90% CI for the probability that 95% CI contains true mean: ", confidence_interval_mean(interval_with_true_mean, 0.1))
+
 
