dd9e6d235b15d3bc94c0009b2b8651ad8ca7a5f7
maxinems <maxinemalekhi@hotmail.com>
2020-09-07 18:10:09 +0700

    first exercise

--- maxine/experimentation/exercise1.py
+++ maxine/experimentation/exercise1.py
@@ -18,15 +18,13 @@ def beta(alpha, beta, draws):
 #null hypothesis to conclude a strong evidence of rejection of null hypothesis
 alpha = 0.1
 a = beta(1.8, 1, 100)
-t_test, pval = stats.ttest_1samp(simulation, 2)
+t_test, pval = stats.ttest_1samp(a, 2)
 if pval > alpha:
     print("accept H0")
 else:
     print("reject H0")
 
-power_one_sample = sm.stats.TTestPower()
-result = power_one_sample.solve_power(nobs1=100, alpha=0.1, effect_size=0.8, alternative='smaller')
-print("The probability of rejecting the null hypothesis is ", result)
+print("The probability of rejecting the null hypothesis is ", 1-pval)
 
 
 #Type 1 error is when H0 is rejected when it should not be (H0 is true), it is related to the reliability of the test (alpha),
