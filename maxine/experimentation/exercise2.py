e63716455e28693e5caadacce7668347d13c5fbc
maxinems <maxinemalekhi@hotmail.com>
2020-09-16 20:56:40 +0700

    revised A/B splitting

--- maxine/experimentation/exercise2.py
+++ maxine/experimentation/exercise2.py
@@ -16,49 +16,47 @@ import numpy as np
 import scipy.stats as stats
 import statsmodels.api as sm
 
-def simulate_users(total_user, pois_a, pois_b):
+def simulate_users(total_user, pois_a, pois_b, weight_a):
     user_a = np.random.poisson(lam=pois_a, size=total_user)
     user_b = np.random.poisson(lam=pois_b, size=total_user)
     user_uniform = np.random.uniform(size=total_user)
-    return np.where(user_uniform < 0.99, user_a, user_b)
+    return np.where(user_uniform < weight_a, user_a, user_b)
 
 
-def split_traffic(users, probability):
-    traffic_a = int(probability*len(users))
-    traffic_b = int((1-probability)*len(users))
-    test_a = np.random.choice(users, size=traffic_a)
-    test_b = np.random.choice(users, size=traffic_b)
+def split_traffic(users):
+    variants = np.random.choice(['a', 'b'], size=len(users))
+    test_a = users * (variants == 'a')
+    test_b = users * (variants == 'b')
     return test_a, test_b
 
 
-def pval(users):
-    test_a, test_b = split_traffic(users, 0.5)
+def pval_booking(users):
+    test_a, test_b = split_traffic(users)
     actions_in_a = sum(test_a)
     actions_in_b = sum(test_b)
     p_val = stats.nbinom.cdf(actions_in_b, actions_in_a, 0.5)
     return p_val
 
 
-def pval_bookers(total_users):
-    """if we can observe non-bookers"""
-    test_a, test_b = split_traffic(simulate_users(total_users, 1.0, 5.0), 0.5)
-    actions_in_a = sum(test_a[test_a>0]) / len(test_a)
-    actions_in_b = sum(test_b[test_b>0]) / len(test_b)
+def pval_bookers(users):
+    """if we observe on bookers"""
+    test_a, test_b = split_traffic(users)
+    actions_in_a = sum(test_a>0)
+    actions_in_b = sum(test_b>0)
     p_val = stats.nbinom.cdf(actions_in_b, actions_in_a, 0.5)
     return p_val
 
-
-def power(effect_size, sample1, alpha):
-    return sm.stats.TTestIndPower().power(effect_size, sample1, alpha)
+def power(pvals, alpha):
+    return np.mean([pval > (1 - alpha) for pval in pvals])
 
 
 def trim(data, top_percent):
-    return sorted(data)[:-int(top_percent*len(data))]
+    return data < np.percentile(data, top_percent)
 
 
 def pval_welch_booker(users):
     """if we can observe non-bookers"""
-    test_a, test_b = split_traffic(users, 0.5)
+    test_a, test_b = split_traffic(users)
     t_stat, p_val = stats.ttest_ind(test_a, test_b, equal_var=False)
     return p_val
 
@@ -66,46 +64,48 @@ def effect_size(test_a, test_b):
     return np.mean(test_a) - np.mean(test_b) / np.sqrt((np.std(test_a)**2 + np.std(test_b)**2)/2)
     
 
-def pval_welch_d(user_a, user_b):
-    test_a = split_traffic(user_a, 0.5)
-    test_b = split_traffic(user_b, 0.5)
-    t_stat, p_val = stats.ttest_ind(test_a, test_b, equal_var=False)
-    test_power = power(effect_size(test_a, test_b), len(test_a), 0.05)
-    return p_val, test_power
+def pval_welch_two_users(user_a, user_b):
+    test_a, test_b = split_traffic(user_a)
+    another_test_a, another_test_b = split_traffic(user_b)
+    t_stat, p_val = stats.ttest_ind(test_a, another_test_b, equal_var=False)
+    return p_val
 
-nsims = 10000
+nsims = 1000
 
 #i
-pvals = [pval(simulate_users(10000, 1, 5)) for _ in range(nsims)]
+pvals = [pval_booking(simulate_users(10000, 1, 5, 0.99)) for _ in range(nsims)]
 plt.figure()
 plt.hist(pvals, bins=100)
 plt.savefig('exp2.png')
 
 #ii
-pvals_bookers = [pval_bookers(simulate_users(10000, 1, 5)) 
+pvals_bookers = [pval_bookers(simulate_users(10000, 1, 5, 0.99)) 
                  for _ in range(nsims)]
 plt.figure()
 plt.hist(pvals_bookers, bins=100)
 plt.savefig('exp2_ii.png')
 
 #iii
-pvals_welch = [pval_welch_booker(simulate_users(10000, 1, 5)) 
+pvals_welch = [pval_welch_booker(simulate_users(10000, 1, 5, 0.99)) 
                for _ in range(nsims)]
 plt.figure()
 plt.hist(pvals_welch, bins=100)
 plt.savefig('exp2_iii.png')
 
 #iv
-print("Power of test is ", pval_welch_d(simulate_users(10000, 1, 5),
-                                        simulate_users(10000, 1.03, 5))[1])
+pvals_two_users = [pval_welch_two_users(simulate_users(10000, 1, 5, 0.99),
+                                        simulate_users(10000, 1.03, 5, 0.99))
+                   for _ in range(nsims)]
+print("Power of test is ", power(pvals_two_users, 0.05))
 
 #v
-pvals_trimmed = [pval_welch_booker(trim(simulate_users(10000, 1, 5), 0.02))
-                 for _ in range(nsims)]
+pvals_trimmed = [pval_welch_two_users(trim(simulate_users(10000, 1, 5, 0.99),
+                                           98),
+                                      trim(simulate_users(10000, 1.03, 5,
+                                                          0.99), 98)) for _ in range(nsims)]
+print("Power of test after trimmed is ", power(pvals_trimmed, 0.05))
+
 plt.figure()
 plt.hist(pvals_trimmed, bins=100)
 plt.savefig('exp2_v_1.png')
 
-print("Power of test after trimmed is ", pval_welch_d(trim(
-    simulate_users(10000, 1, 5), 0.02), 
-    trim(simulate_users(10000, 1.03, 5), 0.02))[1])
